45c6c4d13e4d4002d87d26f0c179d61f0876526b
jude098 <jude098@users.noreply.github.com>
2019-05-17 09:31:02 -0400

    Update dataset-processor.py

--- dataset-processor.py
+++ dataset-processor.py
@@ -29,50 +29,34 @@ print(wine.describe())
 
 
 #visualize the data
+import numpy as np
 import pandas as pd
 import os
-
-
-import matplotlib
-matplotlib.use("TkAgg")
-import matplotlib.pyplot as plt
-
 
 def pretty_print(name, to_print):
     print(f'{name}:')
     print(f'{to_print}\n\n')
 
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
 
-# Loading dataset
-wine = pd.read_csv(filepath_or_buffer='/.../downloads/winedata2.csv',
+wine = pd.read_csv(filepath_or_buffer='/users/.../downloads/winedata2.csv',
                       sep=',',
                       header=0)
 
-
-#Creating plots
-os.makedirs('plots', exist_ok=True)
-
-## Plotting line chart
 pretty_print("wine dataframe", wine.to_string())
 pretty_print("wine columns", wine.columns)
 
-import os
 
 os.makedirs('plots', exist_ok=True)
 
-# Plotting line chart
-plt.plot(wine['Wine'], color='red')
-plt.title('Wine by Alcohol')
-plt.xlabel('Wine')
+#Plotting line chart
+
+plt.plot(wine['Alcohol'], color='green')
+plt.title('Alcohol by Index')
+plt.xlabel('Index')
 plt.ylabel('Alcohol')
-plt.savefig(f'plots/wine_by_alcohol_plot.png', format='png')
-plt.clf()
-#
-#
-#
-# # Plotting scatterplot
-# plt.scatter(housing_df['rm'], housing_df['tax'], color='b')
-# plt.title('rm to tax')
-# plt.xlabel('rm')
-# plt.ylabel('tax')
-# plt.savefig(f'plots/charges_to_tax.png', format='png')
+plt.show()
+
+
